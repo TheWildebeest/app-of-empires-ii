fefea43128c4a1939de84f66edd4550eed978401
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-07-19 09:05:59 +0100

    api call added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,31 @@ import NavBar from './components/NavBar'
 import './App.css';
 
 class App extends Component {
-  render() {
+
+  state = {
+    apiData: null,
+    civilization: null,
+    unit: null,
+    building: null,
+    taunt: null
+  }
+
+  getApiData = (urlParameter) => {
     return (
-      <NavBar />
+      fetch(`https://secret-ocean-49799.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/${urlParameter}`)
+        .then(response => response)
+        .then(response => response.json())
+        .then(data => this.setState({ apiData: data.civilizations }))
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    );
+  }
+
+
+  render() {
+    const { civilization, unit, building, taunt } = this.state;
+    return (
+      <NavBar getApiData={this.getApiData} civilization={civilization} unit={unit} building={building} taunt={taunt} />
     );
   }
 }
