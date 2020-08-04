ad8bd3ffc81ef97771b1e7728b14654d62aa6105
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-08-04 20:00:52 +0100

    removed redundant code cleaned up some of the logic

--- src/components/Civilizations/Civilizations.jsx
+++ src/components/Civilizations/Civilizations.jsx
@@ -7,90 +7,40 @@ import CivsIcon from '../../assets/icons/civs.png';
 
 
 class Civilizations extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     defaultCiv: [{ name: "None found.", unique_unit: ["https://age-of-empires-2-api.herokuapp.com/api/v1/unit/villager"], unique_tech: ["https://age-of-empires-2-api.herokuapp.com/api/v1/technology/wheelbarrow"] }],
-  //     defaultCiv: [{
-  //       "id": 18,
-  //       "name": "None found",
-  //       "expansion": "n/a",
-  //       "army_type": "n/a",
-  //       "unique_unit": [
-  //         "https://age-of-empires-2-api.herokuapp.com/api/v1/unit/villager",
-  //         "https://age-of-empires-2-api.herokuapp.com/api/v1/unit/villager"
-  //       ],
-  //       "unique_tech": [
-  //         "https://age-of-empires-2-api.herokuapp.com/api/v1/technology/wheelbarrow"
-  //       ],
-  //       "team_bonus": "n/a",
-  //       "civilization_bonus": [
-  //         "n/a",
-  //         "n/a",
-  //         "n/a"
-  //       ]
-  //     }],
-  //     targetCiv: {},
-  //     uniqueUnits: { unique_unit: ["..."] },
-  //     uniqueTechs: { unique_tech: ["..."] },
-  //     textVisible: true
-  //   }
-  // }
-
-  state = {
-    defaultCiv: [{
-      "id": 18,
-      "name": "None found",
-      "expansion": "n/a",
-      "army_type": "n/a",
-      "unique_unit": [
-        "https://age-of-empires-2-api.herokuapp.com/api/v1/unit/villager",
-        "https://age-of-empires-2-api.herokuapp.com/api/v1/unit/villager"
-      ],
-      "unique_tech": [
-        "https://age-of-empires-2-api.herokuapp.com/api/v1/technology/wheelbarrow"
-      ],
-      "team_bonus": "n/a",
-      "civilization_bonus": [
-        "n/a",
-        "n/a",
-        "n/a"
-      ]
-    }],
-    targetCiv: {},
-    uniqueUnits: [],
-    uniqueTechs: [""],
-    textVisible: true
+  constructor(props) {
+    super(props);
+    this.state = {
+      defaultCiv: [{
+        name: "",
+        bonus: ["", "", ""],
+        castleAgeTech: "",
+        imperialAgeTech: "",
+        civID: "",
+        uniqueUnits: [""],
+        teamBonus: "",
+        dateModified: [1000000000000]
+      }],
+      targetCiv: {
+        name: "",
+        bonus: ["", "", ""],
+        castleAgeTech: "",
+        imperialAgeTech: "",
+        civID: "",
+        uniqueUnits: [""],
+        teamBonus: "",
+        dateModified: [1596332576807]
+      },
+      textVisible: true
+    }
   }
 
-  // getUniqueUnits = (uniqueUnits, searchTerm) => {
-  //   let uniqueUnitDetails = [];
-  //   if (this.state.targetCiv.name !== "None found" && searchTerm !== "") {
-  //     uniqueUnits.forEach((unit) => {
-  //       fetch(`https://secret-ocean-49799.herokuapp.com/${unit}`)
-  //         .then(response => response.json())
-  //         .then(data => {
-  //           console.log(data);
-  //           let currentUnit = { name: data.name, description: data.description, attackBonus: data.attack_bonus, hitPoints: data.hit_points }
-  //           uniqueUnitDetails.push(currentUnit);
-  //           console.log(uniqueUnitDetails)
-  //         })
-  //         .catch(error => alert(error))
-  //     });
-  //     // this.setState({ uniqueUnits: uniqueUnitDetails });
-  //   } else {
-  //     let currentUnit = { name: "n/a", description: "n/a", attackBonus: 0, hitPoints: 0 }
-  //     uniqueUnitDetails.push(currentUnit);
-  //     // this.setState({ uniqueUnits: uniqueUnitDetails })
-  //   }
-  //   this.setState({ uniqueUnits: uniqueUnitDetails })
-  //   return uniqueUnitDetails;
-  // }
-
+  componentDidMount() {
+    this.props.getApiData("civilizations");
+  }
 
   textFadeIn = () => {
     this.setState({ textVisible: false });
-    setTimeout(() => this.setState({ textVisible: true }), 1500)
+    setTimeout(() => this.setState({ textVisible: true }), 1000)
   }
 
   filterBySearchTerm = (searchTerm) => {
@@ -115,11 +65,6 @@ class Civilizations extends Component {
       currentCiv = this.state.defaultCiv;
     }
     if (currentCiv !== undefined && currentCiv !== null) {
-      if (this.state.targetCiv.name !== "None found" && this.state.targetCiv.unique_unit !== undefined) {
-        const uniqueUnits = this.state.targetCiv.unique_unit;
-        console.log(this.state.targetCiv.unique_unit);
-        // this.getUniqueUnits(uniqueUnits, searchTerm);
-      };
       this.setState({
         targetCiv: currentCiv[0]
       });
@@ -129,36 +74,26 @@ class Civilizations extends Component {
       });
     }
     this.textFadeIn();
-    // if (this.state.targetCiv.name !== "None found" && this.state.targetCiv.unique_unit !== undefined) {
-    //   const uniqueUnits = this.state.targetCiv.unique_unit;
-    //   console.log(this.state.targetCiv.unique_unit);
-    //   this.getUniqueUnits(uniqueUnits, searchTerm);
-    // };
   }
 
   render() {
     const { civilizations } = this.props;
-    const { targetCiv } = this.state;
+    const { targetCiv, defaultCiv } = this.state;
     return (
-      <>
-        <Overlay>
-          <PageHeading pageHeadingText={"Civilizations"} />
-          <InputBox
-            id={"civilization"} type={"text"} placeholder={"Search for a civilization..."}
-            htmlFor={"civilization"} iconUrl={CivsIcon} altText={"civilizations"}
-            autoComplete={"off"} autoFocus={true} civilizations={civilizations} inputHandler={this.searchCiv} />
-          <ContentCard
-            category={"civilizationsPage"}
-            cardName={!targetCiv ? "" : targetCiv.name}
-            subHeading={"Unique Units:"}
-            textVisible={this.state.textVisible}
-            textFadeIn={this.state.textFadeIn}
-            unitsData={this.state.uniqueUnits}>
-            <li>HALLOOORRR</li>
-          </ContentCard>
-
-        </Overlay>
-      </>
+      <Overlay>
+        <PageHeading pageHeadingText={"Civilizations"} />
+        <InputBox
+          id={"civilization"} type={"text"} placeholder={"Search for a civilization..."}
+          htmlFor={"civilization"} iconUrl={CivsIcon} altText={"civilizations"}
+          autoComplete={"off"} autoFocus={true} civilizations={civilizations} inputHandler={this.searchCiv} />
+        <ContentCard
+          // "category" is to allow dynamic styling depending on which parent element renders the card
+          category={"civilizationsPage"}
+          cardDetails={!targetCiv ? defaultCiv : targetCiv}
+          subHeading={"Unique Units:"}
+          textVisible={this.state.textVisible}
+          textFadeIn={this.state.textFadeIn} />
+      </Overlay>
     );
   }
 }
